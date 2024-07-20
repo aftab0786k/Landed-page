4ac69b5e5ada7aceb3a92549154370ad22da95f9
aftab0786k <aftabmiya579@gmail.com>
2024-07-20 15:31:24 +0530

    this is my file

---
+++ src/pages/Desktop.jsx
@@ -0,0 +1,268 @@
+import styles from "./Desktop.module.css";
+
+const Desktop = () => {
+  return (
+    <div className={styles.desktop62}>
+      <div className={styles.desktop62Child} />
+      <img className={styles.subtractIcon} alt="" src="/subtract.svg" />
+      <div className={styles.desktop62Item} />
+      <div className={styles.desktop62Inner} />
+      <div className={styles.empoweringTheDigitalContainer}>
+        <p
+          className={styles.empoweringTheDigital}
+        >{`Empowering the Digital Era: `}</p>
+        <p className={styles.empoweringTheDigital}>
+          Decentarlizing Entertainment
+        </p>
+      </div>
+      <div className={styles.joinTheWorlds}>
+        Join the world’s first DePIN: Revolutionizing Content Streaming with
+        faster, Safer and Cheaper Access
+      </div>
+      <div className={styles.fancloudPioneersA}>
+        FanCloud pioneers a new era of connectivity with its decentralised
+        content delivery network (dCDN), leveraging a global network of
+        community-powered nodes. By harnessing a decentralised architecture,
+        dCDN drastically reduces data transfer and compute costs while enhancing
+        load time. Join FanCloud’s dCDN network and experience the future of
+        seamless, secure and community- driven consumption
+      </div>
+      <div className={styles.fancloudPioneersA1}>
+        FanCloud pioneers a new era of connectivity with its decentralised
+        content delivery network (dCDN), leveraging a global network of
+        community-powered nodes. By harnessing a decentralised architecture,
+        dCDN drastically reduces data transfer and compute costs while enhancing
+        load time. Join FanCloud’s dCDN network and experience the future of
+        seamless, secure and community- driven consumption
+      </div>
+      <img className={styles.groupIcon} alt="" src="/group-427319595.svg" />
+      <div className={styles.rectangleParent}>
+        <div className={styles.groupChild} />
+        <div className={styles.vuesaxboldarrowRightParent}>
+          <img
+            className={styles.vuesaxboldarrowRightIcon}
+            alt=""
+            src="/vuesaxboldarrowright.svg"
+          />
+          <div className={styles.comingSoon}>Coming Soon</div>
+        </div>
+      </div>
+      <div className={styles.patternParent}>
+        <img className={styles.patternIcon} alt="" src="/pattern.svg" />
+        <div className={styles.contentNodes}>Content Nodes</div>
+        <div className={styles.contentNodesEarn}>
+          Content Nodes, Earn rewards for supplying their GPU, Storage and
+          bandwidth for dCDN
+        </div>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.groupItem} />
+          <div className={styles.groupInner} />
+          <div className={styles.vuesaxboldarrowRightGroup}>
+            <img
+              className={styles.vuesaxboldarrowRightIcon1}
+              alt=""
+              src="/vuesaxboldarrowright1.svg"
+            />
+            <div className={styles.comingSoon}>Deploy</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.patternGroup}>
+        <img className={styles.patternIcon1} alt="" src="/pattern1.svg" />
+        <div className={styles.orchestrators}>Orchestrators</div>
+        <div className={styles.orchestratorsWillBe}>
+          Orchestrators will be responsible for connecting the client with the
+          content node for seamless streaming. Traffic distribution will be
+          based on multiple factors like job type, performance
+        </div>
+        <img
+          className={styles.frameChild}
+          alt=""
+          src="/frame-512918690@2x.png"
+        />
+      </div>
+      <div className={styles.patternContainer}>
+        <img className={styles.patternIcon} alt="" src="/pattern2.svg" />
+        <div className={styles.orchestrators}>Validators</div>
+        <div className={styles.orchestratorsWillBe}>
+          Validators will be responsible for validating the execution of dCDN
+          Network. Validators will earn rewards from Staking and earn a cut on
+          the executions validated.
+        </div>
+        <img
+          className={styles.frameChild}
+          alt=""
+          src="/frame-5129186901@2x.png"
+        />
+      </div>
+      <div className={styles.fancloudCdnNetwork}>
+        FanCloud CDN network revolutionizes video infrastructure with
+        community-driven Innovation
+      </div>
+      <div className={styles.theTransitionCdnDcdn}>The transition CDN-dCDN</div>
+      <img className={styles.subtractIcon1} alt="" src="/subtract1.svg" />
+      <img className={styles.globeIcon} alt="" src="/globe@2x.png" />
+      <img className={styles.ellipseIcon} alt="" src="/ellipse-2712@2x.png" />
+      <img
+        className={styles.desktop62Child1}
+        alt=""
+        src="/ellipse-2713@2x.png"
+      />
+      <img
+        className={styles.desktop62Child2}
+        alt=""
+        src="/ellipse-2714@2x.png"
+      />
+      <img
+        className={styles.desktop62Child3}
+        alt=""
+        src="/ellipse-2715@2x.png"
+      />
+      <img
+        className={styles.desktop62Child4}
+        alt=""
+        src="/ellipse-2716@2x.png"
+      />
+      <img
+        className={styles.desktop62Child5}
+        alt=""
+        src="/ellipse-2717@2x.png"
+      />
+      <img
+        className={styles.desktop62Child6}
+        alt=""
+        src="/ellipse-2721@2x.png"
+      />
+      <img
+        className={styles.desktop62Child7}
+        alt=""
+        src="/ellipse-2722@2x.png"
+      />
+      <img
+        className={styles.desktop62Child8}
+        alt=""
+        src="/ellipse-2719@2x.png"
+      />
+      <img
+        className={styles.desktop62Child9}
+        alt=""
+        src="/ellipse-2718@2x.png"
+      />
+      <img
+        className={styles.desktop62Child10}
+        alt=""
+        src="/ellipse-2724@2x.png"
+      />
+      <img className={styles.page1Icon} alt="" src="/page-1@2x.png" />
+      <img className={styles.page1Icon1} alt="" src="/page-11@2x.png" />
+      <div className={styles.ellipseDiv} />
+      <div className={styles.desktop62Child11} />
+      <div className={styles.desktop62Child12} />
+      <div className={styles.desktop62Child13} />
+      <img
+        className={styles.desktop62Child14}
+        alt=""
+        src="/group-427319598.svg"
+      />
+      <img
+        className={styles.desktop62Child15}
+        alt=""
+        src="/group-427319599.svg"
+      />
+      <div className={styles.desktop62Child16} />
+      <div className={styles.desktop62Child17} />
+      <img className={styles.vectorIcon} alt="" src="/vector-987.svg" />
+      <img className={styles.heartIcon} alt="" src="/heart.svg" />
+      <div className={styles.gridLines}>
+        <img
+          className={styles.horizontalLineIcon}
+          alt=""
+          src="/horizontal-line.svg"
+        />
+        <img
+          className={styles.verticalLinesIcon}
+          alt=""
+          src="/vertical-lines@2x.png"
+        />
+        <div className={styles.diagonalLines}>
+          <img className={styles.diagonalLinesChild} alt="" />
+          <img className={styles.diagonalLinesItem} alt="" />
+          <img className={styles.diagonalLinesInner} alt="" />
+          <img className={styles.lineIcon} alt="" />
+          <img className={styles.diagonalLinesChild1} alt="" />
+          <img className={styles.diagonalLinesChild2} alt="" />
+          <img className={styles.diagonalLinesChild3} alt="" />
+          <img className={styles.diagonalLinesChild4} alt="" />
+          <img className={styles.diagonalLinesChild5} alt="" />
+          <img className={styles.diagonalLinesChild6} alt="" />
+          <img className={styles.diagonalLinesChild7} alt="" />
+          <img className={styles.diagonalLinesChild8} alt="" />
+          <img className={styles.diagonalLinesChild9} alt="" />
+          <img className={styles.diagonalLinesChild10} alt="" />
+          <img className={styles.diagonalLinesChild11} alt="" />
+          <img className={styles.diagonalLinesChild12} alt="" />
+          <img className={styles.diagonalLinesChild13} alt="" />
+          <img className={styles.diagonalLinesChild14} alt="" />
+          <img className={styles.diagonalLinesChild15} alt="" />
+          <img className={styles.diagonalLinesChild16} alt="" />
+          <img className={styles.diagonalLinesChild17} alt="" />
+          <img className={styles.diagonalLinesChild18} alt="" />
+          <img className={styles.diagonalLinesChild19} alt="" />
+          <img className={styles.diagonalLinesChild20} alt="" />
+          <img className={styles.diagonalLinesChild21} alt="" />
+          <img className={styles.diagonalLinesChild22} alt="" />
+          <img className={styles.diagonalLinesChild23} alt="" />
+          <img className={styles.diagonalLinesChild24} alt="" />
+        </div>
+        <div className={styles.diagonalLines1}>
+          <img className={styles.diagonalLinesChild} alt="" />
+          <img className={styles.diagonalLinesItem} alt="" />
+          <img className={styles.diagonalLinesInner} alt="" />
+          <img className={styles.lineIcon} alt="" />
+          <img className={styles.diagonalLinesChild1} alt="" />
+          <img className={styles.diagonalLinesChild2} alt="" />
+          <img className={styles.diagonalLinesChild3} alt="" />
+          <img className={styles.diagonalLinesChild4} alt="" />
+          <img className={styles.diagonalLinesChild5} alt="" />
+          <img className={styles.diagonalLinesChild6} alt="" />
+          <img className={styles.diagonalLinesChild7} alt="" />
+          <img className={styles.diagonalLinesChild8} alt="" />
+          <img className={styles.diagonalLinesChild9} alt="" />
+          <img className={styles.diagonalLinesChild10} alt="" />
+          <img className={styles.diagonalLinesChild11} alt="" />
+          <img className={styles.diagonalLinesChild12} alt="" />
+          <img className={styles.diagonalLinesChild13} alt="" />
+          <img className={styles.diagonalLinesChild14} alt="" />
+          <img className={styles.diagonalLinesChild15} alt="" />
+          <img className={styles.diagonalLinesChild16} alt="" />
+          <img className={styles.diagonalLinesChild17} alt="" />
+          <img className={styles.diagonalLinesChild18} alt="" />
+          <img className={styles.diagonalLinesChild19} alt="" />
+          <img className={styles.diagonalLinesChild20} alt="" />
+          <img className={styles.diagonalLinesChild21} alt="" />
+          <img className={styles.diagonalLinesChild22} alt="" />
+          <img className={styles.diagonalLinesChild23} alt="" />
+          <img className={styles.diagonalLinesChild24} alt="" />
+        </div>
+        <img className={styles.blobIcon} alt="" src="/blob.svg" />
+      </div>
+      <div className={styles.groupParent}>
+        <img className={styles.frameInner} alt="" src="/group-427319596.svg" />
+        <div className={styles.noOfNode}>No. of Node Operators</div>
+        <div className={styles.div}>2000</div>
+        <div className={styles.frameChild1} />
+      </div>
+      <div className={styles.dataTransferredInTbLast3Parent}>
+        <div className={styles.noOfNode}>
+          <span>{`Data Transferred in TB `}</span>
+          <span className={styles.last30Days}>(last 30 days)</span>
+        </div>
+        <img className={styles.frameInner} alt="" src="/group-427319596.svg" />
+        <div className={styles.frameChild3} />
+        <div className={styles.div}>12,000</div>
+      </div>
+    </div>
+  );
+};
+
+export default Desktop;
